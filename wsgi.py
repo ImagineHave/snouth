78390892b06f6d7e8b59d8b98d883ff3a9e112da
cwih <chris.williams@imagine-have.xyz>
2018-04-19 19:37:51 +0000

    wsgi test

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 application = get_wsgi_application()
